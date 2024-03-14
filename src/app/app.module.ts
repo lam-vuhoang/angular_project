d80d1a4c502a24d804db880d1298cd0aab8868fe
lam-vuhoang <lam.vuhoang@nashtechglobal.com>
2024-03-14 14:09:55 +0700

    Add Skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShopComponent } from './shop/shop.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { FooterComponent } from './footer/footer.component';
+import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShopComponent,
+    RecipeComponent,
+    FooterComponent,
+    RecipeListComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule
